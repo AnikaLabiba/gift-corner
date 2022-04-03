c3b82720d7447079f505fb2fd52348360e176961
AnikaLabiba <anikalabibajahin@gmail.com>
2022-04-03 22:30:58 +0600

    first commit

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { NavLink, useLocation } from 'react-router-dom';
+import './Header.css'
+
+const Header = () => {
+    const { pathname } = useLocation()
+    return (
+        <div>
+            <Navbar
+                className='navbar' collapseOnSelect expand="lg" variant="light" style={pathname.includes('blog') ? { display: 'none' } : { display: 'flex' }}>
+                <Container >
+                    <NavLink className='logo' to='/'>Gift Corner</NavLink>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+
+                        <Nav className='nav-item'>
+                            <NavLink
+                                className={({ isActive }) => (isActive ? "active-link" : "link")}
+                                to='/'
+                            >
+                                Home
+                            </NavLink>
+                            <NavLink
+                                className={({ isActive }) => (isActive ? "active-link" : "link")}
+                                to='/reviews'
+                            >
+                                Reviews
+                            </NavLink>
+                            <NavLink
+                                className={({ isActive }) => (isActive ? "active-link" : "link")}
+                                to='/blog'
+                            >
+                                Blog
+                            </NavLink>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </div>
+    );
+};
+
+export default Header;
